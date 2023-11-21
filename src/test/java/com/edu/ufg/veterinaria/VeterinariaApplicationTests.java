481fa70f05cd7dc796901b3ca65fed610ac32f27
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-21 10:22:10 -0600

    initial commit

---
+++ src/test/java/com/edu/ufg/veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ufg.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
