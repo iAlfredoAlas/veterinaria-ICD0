f247ed62af1f6bec9619f917f20469a96f79bdce
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-22 10:28:08 -0600

    Add Departamento model

---
+++ src/main/java/com/edu/ufg/veterinaria/models/Departamento.java
@@ -0,0 +1,26 @@
+package com.edu.ufg.veterinaria.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "departamento")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Departamento {
+
+    @Id
+    @Column(name = "id_departamento")
+    private Long idDepartamento;
+
+    @Column(name = "nombre_departamento")
+    @Basic(optional = false)
+    private String nombreDepartamento;
+
+}
