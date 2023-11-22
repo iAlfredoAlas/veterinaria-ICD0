13c6297ad1365178f899fd1d9465039b9779e075
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-22 11:03:40 -0600

    Add CategoriaMascota model

---
+++ src/main/java/com/edu/ufg/veterinaria/models/CategoriaMascota.java
@@ -0,0 +1,32 @@
+package com.edu.ufg.veterinaria.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria_mascota", schema = "catalogo")
+@SQLDelete(sql = "UPDATE catalogo.categoria_mascota SET estado_categoria_mascota = false WHERE id_categoria_mascota=?")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoriaMascota {
+
+    @Id
+    @Column(name = "id_categoria_mascota")
+    private Long idCategoriaMascota;
+
+    @Column(name = "nombre_categoria_mascota")
+    @Basic(optional = false)
+    private String nombreCategoriaMascota;
+
+    @Column(name = "estado_categoria_mascota")
+    @Basic(optional = false)
+    private Boolean estado_categoria_mascota = Boolean.TRUE;
+
+}
