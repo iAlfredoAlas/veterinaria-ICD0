b5c1319f1d8433ddd73bea435e7260ae248935dd
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-24 16:00:03 -0600

    Add DepartamentoDTO

---
+++ src/main/java/com/edu/ufg/veterinaria/models/dto/DepartamentoDTO.java
@@ -0,0 +1,23 @@
+package com.edu.ufg.veterinaria.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DepartamentoDTO {
+
+    private Long idDepartamento;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ][a-zA-ZáéíóúÁÉÍÓÚñÑ,. ]{0,19}+$", message = "El campo solo puede tener como máximo 20 caracteres alfabéticos")
+    private String nombreDepartamento;
+
+}
