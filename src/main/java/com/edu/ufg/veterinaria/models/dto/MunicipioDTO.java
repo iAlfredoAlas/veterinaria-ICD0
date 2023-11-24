5a4cc64dc6667ea32932cfbf853c9c257b8af0c7
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-24 16:00:16 -0600

    Add MunicipioDTO

---
+++ src/main/java/com/edu/ufg/veterinaria/models/dto/MunicipioDTO.java
@@ -0,0 +1,27 @@
+package com.edu.ufg.veterinaria.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MunicipioDTO {
+
+    private Long idMunicipio;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ][a-zA-ZáéíóúÁÉÍÓÚñÑ,. ]{0,24}+$", message = "El campo solo puede tener como máximo 25 caracteres alfabéticos")
+    private String nombreMunicipio;
+
+    @NotNull(message = "El campo solo puede ser null")
+    private DepartamentoDTO idDepartamento;
+
+}
