df7e5b52ec7c48dc151d129cd5e2f3af65bd6bec
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-26 22:02:47 -0600

    Add IDistritoRepository

---
+++ src/main/java/com/edu/ufg/veterinaria/repository/catalogo/IDistritoRepository.java
@@ -0,0 +1,7 @@
+package com.edu.ufg.veterinaria.repository.catalogo;
+
+import com.edu.ufg.veterinaria.models.Distrito;
+import com.edu.ufg.veterinaria.repository.IGenericRepository;
+
+public interface IDistritoRepository extends IGenericRepository<Distrito, Long> {
+}
