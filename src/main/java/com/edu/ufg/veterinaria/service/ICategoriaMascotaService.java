400b2b3de6920670f9614231b40b93e9112f884f
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-26 22:48:58 -0600

    Add ICategoriaMascotaService.java

---
+++ src/main/java/com/edu/ufg/veterinaria/service/ICategoriaMascotaService.java
@@ -0,0 +1,6 @@
+package com.edu.ufg.veterinaria.service;
+
+import com.edu.ufg.veterinaria.models.CategoriaMascota;
+
+public interface ICategoriaMascotaService extends IServiceGeneric<CategoriaMascota, Long> {
+}
