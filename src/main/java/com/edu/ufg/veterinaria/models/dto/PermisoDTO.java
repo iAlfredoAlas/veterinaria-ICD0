e296ed7a8a110b67b1a636c3654c662245288b69
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-26 19:20:23 -0600

    Add PermisoDTO

---
+++ src/main/java/com/edu/ufg/veterinaria/models/dto/PermisoDTO.java
@@ -0,0 +1,35 @@
+package com.edu.ufg.veterinaria.models.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PermisoDTO {
+
+    private Long idPermiso;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ][a-zA-ZáéíóúÁÉÍÓÚñÑ,. ]{0,39}$", message = "El campo solo puede tener como máximo 40 caracteres alfabéticos")
+    private String nombrePermiso;
+
+    @NotNull(message = "El campo solo puede ser VERDADERO o FALSO")
+    private Boolean estadoPermiso = Boolean.TRUE;
+
+    @JsonIgnore
+    private List<RolDTO> rolList = new ArrayList<>();
+
+    private MenuDTO idMenu;
+
+}
