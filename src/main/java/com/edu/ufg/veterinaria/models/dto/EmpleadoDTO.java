5369c9b0115e2e0316378fe674e6415445b1ed18
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-26 19:11:45 -0600

    Add EmpleadoDTO

---
+++ src/main/java/com/edu/ufg/veterinaria/models/dto/EmpleadoDTO.java
@@ -0,0 +1,55 @@
+package com.edu.ufg.veterinaria.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmpleadoDTO {
+
+    private Long idEmpleado;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ][a-zA-ZáéíóúÁÉÍÓÚñÑ,. ]{0,39}$", message = "El campo solo puede tener como máximo 40 caracteres alfabéticos")
+    private String nombreEmpleado;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9][a-zA-ZáéíóúÁÉÍÓÚñÑ0-9,. ]{0,39}$", message = "El campo solo puede tener como máximo 40 caracteres alfabéticos")
+    private String complementoDireccion;
+
+    @Email(message = "El correo debe de ser válido")
+    private String correoEmpleado;
+
+    @NotBlank(message = "EL campo no puede quedar vacío")
+    @Pattern(regexp = "^[0-9]{8,11}$", message = "El campo solo puede tener como máximo 40 caracteres alfabéticos")
+    private String telefonoEmpleado;
+
+    @NotBlank
+    private String contrasena;
+
+    @NotNull(message = "El campo solo puede ser VERDADERO o FALSO")
+    private Boolean estadoEmpleado = Boolean.TRUE;
+
+    @NotNull(message = "El campo solo puede ser null")
+    private EspecialidadMedicaDTO idEspecialidadMedica;
+
+    @NotNull(message = "El campo solo puede ser null")
+    private ClinicaDTO idClinica;
+
+    @NotNull(message = "El campo solo puede ser null")
+    private DistritoDTO idDistito;
+
+    private List<RolDTO> rolList = new ArrayList<>();
+
+}
