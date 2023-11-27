826102ed9cbe7838803bccb94595d912d881c8f4
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-26 23:22:47 -0600

    Add new method

--- src/main/java/com/edu/ufg/veterinaria/repository/administracion/IMenuRepository.java
+++ src/main/java/com/edu/ufg/veterinaria/repository/administracion/IMenuRepository.java
@@ -2,11 +2,16 @@ package com.edu.ufg.veterinaria.repository.administracion;
 
 import com.edu.ufg.veterinaria.models.Menu;
 import com.edu.ufg.veterinaria.repository.IGenericRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IMenuRepository extends IGenericRepository<Menu, Long> {
 
     List<Menu> findByEstadoMenu (Boolean estadoMenu);
+
+    @Query("SELECT mn FROM Menu mn WHERE mn.idMenuSuperior is null")
+    List<Menu> findBySuperiorMenus();
 
 }
