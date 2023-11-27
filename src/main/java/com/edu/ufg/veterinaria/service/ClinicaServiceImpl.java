a61c62b25114ecab01dd645ab6d926d3d9ea387c
Alfredo Alas <alfredo.alas14@gmail.com>
2023-11-27 15:03:03 -0600

    Add ClinicaServiceImpl.java

---
+++ src/main/java/com/edu/ufg/veterinaria/service/ClinicaServiceImpl.java
@@ -0,0 +1,59 @@
+package com.edu.ufg.veterinaria.service;
+
+import com.edu.ufg.veterinaria.models.Clinica;
+import com.edu.ufg.veterinaria.repository.clinica.IClinicaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ClinicaServiceImpl implements IClinicaService {
+
+    @Autowired
+    private IClinicaRepository clinicaRepository;
+
+
+    @Override
+    public Page<Clinica> getAll(Pageable pageable) {
+        log.info("Show all data");
+        return clinicaRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Clinica> findCustom(Pageable pageable, Boolean flat) {
+        log.info("Show actives");
+        return clinicaRepository.findByEstadoClinica(pageable, flat);
+    }
+
+    @Override
+    public Clinica findById(Long id) {
+        log.info("Show by id");
+        return clinicaRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException("Not found", 404));
+    }
+
+    @Override
+    public Clinica add(Clinica model) {
+        return clinicaRepository.save(model);
+    }
+
+    @Override
+    public Clinica update(Clinica model, Long id) {
+        log.info("Save data");
+        Clinica objClinica = clinicaRepository.findById(id).get();
+        objClinica.setNombreClinica(model.getNombreClinica());
+        objClinica.setComplementoDireccionClinica(model.getComplementoDireccionClinica());
+        objClinica.setEstadoClinica(model.getEstadoClinica());
+        objClinica.setIdDistrito(model.getIdDistrito());
+        return clinicaRepository.save(objClinica);
+    }
+
+    @Override
+    public void deleteLog(Long id) {
+        log.info("Update data");
+        clinicaRepository.deleteById(id);
+    }
+}
